d9203f99d348a8cc77c9335b4d6a61815f902d1c
Sonika Choudhary <Sonikachoudhary2006@gmail.com>
2026-08-31 18:12:06 +0530

    Day 11: Count Even and Odd Elements

---
+++ day11_even_odd_count.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int even = 0, odd = 0;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+
+        if (arr[i] % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    cout << "Even elements = " << even << endl;
+    cout << "Odd elements = " << odd << endl;
+
+    return 0;
+}
